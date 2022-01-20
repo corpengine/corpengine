fa99760aff21910f65a0c16d8c7ec3a2751d545a
PyxleDev0 <pyxledev0@gmail.com>
2022-01-20 11:14:55 +0300

    update version to 0.7.0

--- corpengine.py
+++ corpengine.py
@@ -35,7 +35,7 @@ colors = Colors()
 # CONSTANTS MODULE
 class Constants:
     def __init__(self) -> None:
-        self.ENGINEVERSION: str = '0.6.3b'
+        self.ENGINEVERSION: str = '0.7.0'
         self.DEFAULTSCREENSIZE: tuple = (640, 360)
         self.WINDOWTITLE: str = 'CORP Engine window'
         self.FLAGS: int
