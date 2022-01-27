dace367e6d842b8451190b5999333d54a03ba328
PyxleDev0 <pyxledev0@gmail.com>
2022-01-27 18:47:39 +0300

    GameObject root object conversion for all GameObjects

--- example_window.py
+++ example_window.py
@@ -1,6 +1,17 @@
-import corpengine
-from corpengine import flags
+import corpengine as corp
 
-engine = corpengine.init(windowTitle='Example Window', flags=flags.RESIZABLE|flags.SCALED)
+engine = corp.init(flags=corp.flags.SCALED)
+
+class Test(corp.Entity):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def setup(self):
+        corp.TestComponent().add(self)
+
+    def update(self, dt):
+        print(self.foo(5))
+
+engine.game.Object.new(Test(engine.game.Workspace))
 
 engine.mainloop()
